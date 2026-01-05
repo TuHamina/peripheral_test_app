15fc330b6cbec12290e35152a8fb3f46dd06a97b
Tuulikki Hamina <hamina.tuulikki@gmail.com>
2026-01-05 14:03:20 +0200

    Add bzip2 CRC32 implementation and unit test

---
+++ tests/crc32.h
@@ -0,0 +1,12 @@
+#ifndef CRC32_H
+#define CRC32_H
+
+#include <stdint.h>
+
+void BZ2_initialise_crc (uint32_t *crc);
+
+void BZ2_finalise_crc (uint32_t *crc);
+
+void BZ2_update_crc (uint32_t *crc, uint8_t ch);
+
+#endif // CRC32_H
