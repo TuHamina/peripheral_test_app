a4f743124c6491ffd83bac8cb580f8a9f16c8685
Tuulikki Hamina <hamina.tuulikki@gmail.com>
2026-01-20 09:22:54 +0200

    Add shell commands for register read/write access

--- src/shell/shell.c
+++ src/shell/shell.c
@@ -80,6 +80,46 @@ SHELL_CMD_REGISTER(nfctest, NULL,
                    "NFC test command",
                    cmd_nfctest);
 
+static int check_address(const char *s, uintptr_t *out_addr)
+{
+    uintptr_t addr;
+
+    if (out_addr == NULL) 
+    {
+        return -EINVAL;
+    }
+
+    if (s == NULL) 
+    {
+        return -EINVAL;
+    }
+
+    if (strlen(s) < 3) 
+    {
+        return -EINVAL;
+    }
+
+    if (s[0] == '-') 
+    {
+        return -EINVAL;
+    }
+
+    if (!(s[0] == '0' && (s[1] == 'x' || s[1] == 'X')))
+    {
+        return -EINVAL;
+    }
+
+    addr = (uintptr_t)strtoul(s, NULL, 16);
+
+    if ((addr & 0x3) != 0) 
+    {
+        return -EINVAL;
+    }
+
+    *out_addr = addr;
+    return 0;
+}
+
 static int cmd_crc32(const struct shell *sh, size_t argc, char **argv)
 {
     uintptr_t address;
@@ -92,13 +132,18 @@ static int cmd_crc32(const struct shell *sh, size_t argc, char **argv)
         return -1;
     }
 
+    if (check_address(argv[1], &address) != 0)
+    {
+        shell_error(sh, "Invalid address");
+        return -EINVAL;
+    }
+
     if (argv[2][0] == '-') 
     {
         shell_print(sh, "Word count must be positive");
         return -EINVAL;
     }
 
-    address   = strtoul(argv[1], NULL, 0);
     words_len = strtoul(argv[2], NULL, 0);
 
     if (strcmp(argv[3], "0") == 0)
@@ -139,3 +184,90 @@ static int cmd_crc32(const struct shell *sh, size_t argc, char **argv)
 SHELL_CMD_REGISTER(crc32, NULL,
                    "crc32 test command",
                    cmd_crc32);
+
+static void mem32_read(uintptr_t addr, uint32_t num,
+                       const struct shell *sh)
+{
+    volatile uint32_t *p = (volatile uint32_t *)addr;
+    printk("num=%d\n", num);
+    for (uint32_t i = 0; i < num; i++)
+    {
+        shell_print(sh,
+            "0x%08X = 0x%08X, %d",
+            (uint32_t)&p[i],
+            p[i], i);
+    }
+}
+
+static int cmd_mem32(const struct shell *sh,
+                     size_t argc,
+                     char **argv)
+{
+    if (argc != 3)
+    {
+        shell_error(sh,
+            "Usage: mem32 <addr> <num>");
+        return -EINVAL;
+    }
+
+    uintptr_t addr;
+    if (check_address(argv[1], &addr) != 0)
+    {
+        shell_error(sh, "Invalid address");
+        return -EINVAL;
+    }
+
+    if (argv[2][0] == '-')
+    {
+        shell_error(sh, "Value must be positive");
+        return -EINVAL;
+    }
+
+    uint32_t num = strtoul(argv[2], NULL, 0);
+
+    mem32_read(addr, num, sh);
+    return 0;
+}
+
+SHELL_CMD_REGISTER(mem32, NULL,
+    "Read 32-bit memory words",
+    cmd_mem32);
+
+static int cmd_w4(const struct shell *sh,
+                     size_t argc,
+                     char **argv)
+{
+    if (argc != 3)
+    {
+        shell_error(sh,
+            "Usage: w4 <addr> <value>");
+        return -EINVAL;
+    }
+
+    uintptr_t addr;
+    if (check_address(argv[1], &addr) != 0)
+    {
+        shell_error(sh, "Invalid address");
+        return -EINVAL;
+    }
+
+    if (argv[2][0] == '-')
+    {
+        shell_error(sh, "Value must be positive");
+        return -EINVAL;
+    }
+
+    uint32_t value = strtoul(argv[2], NULL, 0);
+
+    volatile uint32_t *p = (volatile uint32_t *)addr;
+    *p = value;
+
+    shell_print(sh, "OK: 0x%08X @ 0x%08X",
+                value, (uint32_t)addr);
+    
+    return 0;
+}
+
+SHELL_CMD_REGISTER(w4, NULL,
+    "Write 32-bit memory word",
+    cmd_w4);
